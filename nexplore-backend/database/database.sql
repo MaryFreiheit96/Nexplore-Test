1a2053c1def7fc304b4578b0bbf7937908de8ec3
MaryFreiheit96 <mary.eiriz@gmail.com>
2022-12-28 10:03:28 +0100

    Add backend and frontend apps

---
+++ nexplore-backend/database/database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE nexplore-test;
+
+CREATE TABLE duties(
+    id PRIMARY KEY, 
+    name VARCHAR(40)
+)
+
+INSERT INTO duties (id,name) VALUES ("111", "Maria")
---
+++ nexplore-backend/src/routes.ts
@@ -0,0 +1,32 @@
+import { DutyService } from "./service";
+import * as http from "http";
+import { pathToRegexp } from "path-to-regexp";
+
+export default async function (
+  req: http.IncomingMessage,
+  res: http.ServerResponse
+) {
+  const dutyService = new DutyService();
+  const url = req.url.split("?")[0];
+
+  if (req.method === "POST" && url === "/create") {
+    await dutyService.createDuty(req, res);
+  } else if (req.method === "GET" && url === "/all") {
+    await dutyService.getAllDuties(res);
+  } else if (req.method === "GET" && url.match(pathToRegexp("/:id"))) {
+    await dutyService.getDuty(req, res);
+  } else if (req.method === "DELETE" && url.match(pathToRegexp("/:id"))) {
+    await dutyService.deleteDuty(req, res);
+  } else if (req.method === "PUT" && url.match(pathToRegexp("/:id"))) {
+    await dutyService.editDuty(req, res);
+  } else {
+    console.log("aqui", req.method);
+    res.writeHead(200, {
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+      "Content-Type": "application/json",
+    });
+    res.end(JSON.stringify({ message: "Not Found" }));
+  }
+}
